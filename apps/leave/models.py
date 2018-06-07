61c78601f5ac52caa4bae7266d306f1ee9603df1
prakarshsaxena <prakarshsaxena31@gmail.com>
2018-06-07 18:10:24 +0530

    leave model adding

--- apps/leave/models.py
+++ apps/leave/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Leave:
+    LEAVE_TYPE_CHOICES = (
+        ('PL', 'Privilege Leave'),
+        ('CL', 'Casual Leave'),
+        ('Half Day', 'Half Day'),
+    )
+
+    leave_type = models.CharField(
+        max_length= 8,
+        choices=LEAVE_TYPE_CHOICES,
+        default='PL',
+    )
