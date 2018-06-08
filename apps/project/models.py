1c05a18a957c458539848c762ecb12ce28d2654d
skzinc <shwetasingh426060@gmail.com>
2018-06-08 14:51:19 +0530

    default change to none in client id

--- apps/project/models.py
+++ apps/project/models.py
@@ -4,7 +4,7 @@ class IT_Project(models.Model):
 
     project_name = models.CharField(max_length=30)
     project_description = models.CharField(max_length=30)
-    client_id = models.IntegerField()
+    client_id = models.IntegerField(default=None)
     project_manager_id = models.IntegerField()
     project_price = models.IntegerField()
     project_start_date_time = models.DateTimeField()
