e2b5f01286088a4a13b54d6e99165bc19048a93b
prakarshsaxena <prakarshsaxena31@gmail.com>
2018-06-08 15:18:02 +0530

    Migrations prakarsh done

--- apps/leave/models.py
+++ apps/leave/models.py
@@ -3,8 +3,8 @@ from ..users.models import MyUser
 
 
 class Leave(models.Model):
-    pl = models.IntegerField(null=True)
-    cl = models.IntegerField(null=True)
-    half_day = models.IntegerField(null= True)
-    comp_off = models.IntegerField(null= True)
-    user = models.OneToOneField(MyUser, on_delete=models.PROTECT, blank=True)
+    pl = models.IntegerField(default=None)
+    cl = models.IntegerField(default=None)
+    half_day = models.IntegerField(default=None)
+    comp_off = models.IntegerField(default=None)
+    user = models.OneToOneField(MyUser, on_delete=models.PROTECT, blank=True, default=None)
