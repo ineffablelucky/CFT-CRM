b3aa95621fca3d0e33794f9fd293772694e7d77f
JhaRajesh <rajeshjha2097@gmail.com>
2018-06-08 13:02:20 +0530

    ..

--- apps/client/models.py
+++ apps/client/models.py
@@ -1,8 +1,8 @@
 from django.db import models
-from apps.users.models import USER
+from apps.users.models import MyUser
 
 
 class CLIENT(models.Model):
 
     company_name = models.CharField(max_length=100)
-    client_user = models.OneToOneField(USER, on_delete=models.PROTECT)
+    client_user = models.OneToOneField(MyUser, on_delete=models.PROTECT)
