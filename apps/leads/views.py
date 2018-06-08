0539013ebaed2cb9a656909f54a69f9c0d2257f0
CFTuser <deveshmaheshwari100@gmail.com>
2018-06-08 14:22:38 +0530

    ..

--- apps/leads/views.py
+++ apps/leads/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, UpdateView, DeleteView
+from .models import LEADS
+from django.contrib.messages.views import SuccessMessageMixin
 
-# Create your views here.
+
+class LeadCreate(SuccessMessageMixin,CreateView):
+    model = LEADS
+    fields = '__all__'
+    template_name = 'leads/create.html'
+    success_message = "lead was created successfully"
+
+
+	#success_url = reverse_lazy('clients:p')
+
+
+class leadedit(UpdateView):
+	model=LEADS
+	template_name = 'leads/update.html'
+	fields = '__all__'
+	# success_url = reverse_lazy('clients:projectdetails')
+
+
+	#def get_success_url(self, **kwargs):
+		#return reverse_lazy('clients:projectdetails', args=(self.object.id,))
+
+
+class leaddelete(DeleteView):
+	model = LEADS
+	success_url = reverse_lazy('clients:p')
+	def get(self, request, *args, **kwargs):
+		return self.post(request, *args, **kwargs)
+
