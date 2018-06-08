34879f9916a393214a372c142f1fa9aa6d515e87
pranjal216 <pranjal216@gmail.com>
2018-06-08 16:56:30 +0530

    salary_calculationns

--- apps/salary_percentages/models.py
+++ apps/salary_percentages/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Salary_calculations(models.Model):
     cal_id = models.AutoField(primary_key=True)
-    hra_percentage = models.IntegerField
-    finanacial_year = models.IntegerField
+    hra_percentage = models.IntegerField()
+    finanacial_year = models.IntegerField()
     allowances =  models.CharField(max_length=45)
-    ppf_percentage =  models.IntegerField
+    ppf_percentage =  models.IntegerField()
--- configs/settings.py
+++ configs/settings.py
@@ -89,12 +89,15 @@ WSGI_APPLICATION = 'configs.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': '',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
