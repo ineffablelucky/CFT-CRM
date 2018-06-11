f2cb6b30236129ff3456c09f37abd9ba9423acf6
prakarshsaxena <prakarshsaxena31@gmail.com>
2018-06-11 17:23:21 +0530

    Merge branch 'master' of https://github.com/ineffablelucky/CFT-CRM
    # Conflicts:
    #	configs/urls.py

--- apps/attendance/views.py
+++ apps/attendance/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 from .forms import LeaveForm
+from django.views.generic import CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-def leave_request(request):
-    form = LeaveForm()
-    return render(request, 'leaverequest.html', {'form' : form})
+class LeaveRequest(LoginRequiredMixin, CreateView):
+    form_class = LeaveForm
+    template_name = 'leaverequest.html'
+
+    def get_form_kwargs(self):
+        """Returns the keyword arguments for instantiating the form"""
+        kwargs = super().get_form_kwargs()
+        kwargs.update({'logged_user': self.request.user})
+        kwargs = {'logged_user' : self.request.user}
+        return kwargs
+
+
 
