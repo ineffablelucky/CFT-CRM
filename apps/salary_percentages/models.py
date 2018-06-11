3fac047f1a8d3936fd50767f2be4a78b5dcb8529
pranjal216 <pranjal216@gmail.com>
2018-06-11 13:09:10 +0530

    Merge branch 'master' of http://github.com/ineffablelucky/CFT-CRM
    # Conflicts:
    #	apps/salary_percentages/models.py

--- apps/salary_percentages/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Salary_calculations(models.Model):
-
-    hra_percentage = models.IntegerField()
-    finanacial_year = models.IntegerField()
-    allowances =  models.CharField(max_length=45)
-    ppf_percentage =  models.IntegerField()
