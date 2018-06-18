c59125ff583f5e898548a0e3a855722cee8d0626
prakarshsaxena <prakarshsaxena31@gmail.com>
2018-06-18 18:00:52 +0530

    model form made to leave request

--- apps/attendance/views.py
+++ apps/attendance/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from .forms import LeaveForm
-from django.views.generic import CreateView, TemplateView
+from django.views.generic import CreateView, TemplateView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import redirect
+from django.http import HttpResponseForbidden, HttpResponse
+from ..leave.models import Leave
+from ..attendance.models import Attendance
 
 import datetime
 from copy import deepcopy
@@ -12,6 +15,8 @@ class LeaveRequest(LoginRequiredMixin, CreateView):
     form_class = LeaveForm
     template_name = 'leaverequest.html'
     success_url = '/'
+    model = Leave
+    context_object_name = 'leave'
 
     def get_form_kwargs(self):
         """Returns the keyword arguments for instantiating the form"""
@@ -53,5 +58,54 @@ class Completed(LoginRequiredMixin, TemplateView):
     template_name = 'complete.html'
 
 
+class Clock(LoginRequiredMixin,TemplateView):
+    template_name = 'clock_in.html'
+
+    def get(self, request, *args, **kwargs):
+        return render(request, 'clock_in.html')
+
+
+class Clockin(LoginRequiredMixin, CreateView):
+    template_name = 'clock_in.html'
+    success_url = '/'
+    """
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs.update({'logged_user': self.request.user})
+        return kwargs
+    """
+    def post(self, request, *args, **kwargs):
+
+        if not request.user.is_authenticated:
+            return HttpResponseForbidden
+        elif Attendance.objects.filter(user_id=self.request.user.id, date=datetime.date.today()):
+            return HttpResponse("Already Clocked In")
+
+        else:
+            a = Attendance.objects.create(user_id=self.request.user.id,
+                                      date=datetime.date.today(),
+                                      time_in=datetime.datetime.today(),
+                                      status='present',
+                                      )
+            a.save()
+            return redirect('/')
+
+
+class Clockout(LoginRequiredMixin, UpdateView):
+    template_name = 'clock_in.html'
+
+    def post(self, request, *args, **kwargs):
+
+        if not request.user.is_authenticated:
+            return HttpResponseForbidden
+
+        else:
+            a = Attendance.objects.get(user_id = self.request.user, date=datetime.date.today())
+            if a is None:
+                return HttpResponse("first CLock In")
+            else:
+                a.time_out = datetime.datetime.today()
+                a.save()
+            return redirect('/')
 
 
