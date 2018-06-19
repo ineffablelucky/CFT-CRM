bd296587849321ace9905ae5bf9e3469167e6c29
pranjal216 <pranjal216@gmail.com>
2018-06-19 09:49:43 +0530

    test commit

--- apps/ctc/models.py
+++ apps/ctc/models.py
@@ -1,10 +1,13 @@
 from django.db import models
+from apps.salary_percentages.models import Salary_calculations
 
-# Create your models here.
+
 class CTC_breakdown(models.Model):
     break_up = models.CharField(max_length=250)
     calculations = models.IntegerField()
     monthly_salary = models.IntegerField()
     yearly_salary = models.IntegerField()
 
+    def basic_sal(self):
+        self.calculations = 0.5*gross
 
--- apps/ctc/views.py
+++ apps/ctc/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import CTC_breakdown
 
-# Create your views here.
+
+def slip(request,id):
+    c=CTC_breakdown.objects.get(id=id)
+    return render(request,'gross_sal.html',{'c':c})
