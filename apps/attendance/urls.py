d077f941e62ff5630aa6fd77c039fbff618a3246
Kapil <kapilarj1997@gmail.com>
2018-06-20 09:39:10 +0530

    Merge branch 'master' of https://github.com/ineffablelucky/CFT-CRM
    # Conflicts:
    #	apps/attendance/urls.py

--- apps/attendance/urls.py
+++ apps/attendance/urls.py
@@ -9,12 +9,7 @@ urlpatterns = [
     #path('clock/', Clock.as_view(), name='clock'),
     path('clock/in/', Clockin.as_view(), name='clockin'),
     path('clock/out/', Clockout.as_view(), name='clockout'),
-<<<<<<< HEAD
     path('userattendance/', PastAttendance.as_view(), name='pastattendance' )
 ]
 
-=======
-    path('userattendance/', PastAttendance.as_view(), name='pastattendance' ),
-    #path('leaverequest/', LeaveTable.as_view(), name = 'leave_request')
-]
->>>>>>> 0a4ad975e1662f23018696dc59e6087a87db3a76
+
