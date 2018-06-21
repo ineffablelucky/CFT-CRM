faf4d101081903be3aa468a8c7126ae6732231dd
pranjal216 <pranjal216@gmail.com>
2018-06-21 18:26:41 +0530

    Merge branch 'master' of http://github.com/ineffablelucky/CFT-CRM
    # Conflicts:
    #	apps/users/templates/welcome.html
    #	configs/urls.py

--- apps/salary_percentages/views.py
+++ apps/salary_percentages/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,HttpResponseRedirect,reverse
 from . import models
 from .models import Salary_calculations
 from .forms import SalaryForm
 from django.views.generic import ListView,CreateView,DetailView,DeleteView,UpdateView,TemplateView
+from django.contrib import messages
 
 def salary_structure(request):
     context = Salary_calculations.objects.all()
@@ -11,8 +12,11 @@ def salary_structure(request):
 def add(request):
     if request.method=="POST":
         form = SalaryForm(request.POST)
-        form.save()
-        return redirect('/salary_structure')
+        if form.is_valid():
+            form.save()
+            return redirect('/salary')
+        else:
+            return HttpResponseRedirect(reverse('salary_percentages:salary_structure'))
     else :
         form = SalaryForm()
         return render(request,'work/salary_struct_form.html',{'form':form})
