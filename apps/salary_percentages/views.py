a2952fef5cf57c1110f6560318faca89353b6571
pranjal216 <pranjal216@gmail.com>
2018-06-26 18:00:31 +0530

    Merge branch 'master' of http://github.com/ineffablelucky/CFT-CRM
    # Conflicts:
    #	apps/users/templates/welcome.html
    #	configs/urls.py

--- apps/salary_percentages/views.py
+++ apps/salary_percentages/views.py
@@ -13,7 +13,7 @@ def edit_salary(request,id):
     if request.method == 'POST':
         form = CtcForm(request.POST)
         form.save()
-        return redirect("{% url 'salary_percentages:edit_salary' id=context.employee_id %}")
+        return HttpResponseRedirect(reverse("salary_percentages:edit_salary"))
     employee_detail_object = Employee_details.objects.get(worker_id=id)
     context=CTC_breakdown.objects.get(employee_id=id)
     return render(request,'work/edit_salary.html',{'context':context, 'employee_detail_object' : employee_detail_object})
