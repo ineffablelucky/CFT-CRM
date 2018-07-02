13689f3f8fbb24a0bee546e13db5e7f09d1c9c87
CFTuser <deveshmaheshwari100@gmail.com>
2018-07-02 18:42:52 +0530

    abc

--- apps/leads/urls.py
+++ apps/leads/urls.py
@@ -9,7 +9,7 @@ urlpatterns =[
     #path('i',views.LeadIndex.as_view(),name='LeadIndex'),
     path('details/',views.LeadDetails.as_view(),name='LeadDetails'),
     path('create/',views.LeadCreate.as_view(),name='LeadCreate'),
-    path('edit/<int:id>',views.leadsedit,name='LeadEdit'),
+    path('edit/<pk>',views.LeadEdit.as_view(),name='LeadEdit'),
     path('delete/<pk>',views.LeadDelete.as_view(),name='LeadDelete'),
     path('details/assign/',views.LeadsAssign,name='leadsassign'),
 
