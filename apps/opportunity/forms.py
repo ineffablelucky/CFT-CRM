50b4f692ca0824d86c9c55a9cff751b01a21a712
JhaRajesh <rajeshjha2097@gmail.com>
2018-07-05 16:08:39 +0530

    some change

--- apps/opportunity/forms.py
+++ apps/opportunity/forms.py
@@ -4,7 +4,6 @@ from django.contrib.auth.models import Group
 from django import forms
 from django.contrib.auth.hashers import make_password
 from django.db.models import Q
-
 from apps.client.models import CLIENT
 from apps.opportunity.models import Opportunity
 from apps.project.models import IT_Project
