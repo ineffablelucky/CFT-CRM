4c9712803dc0d11649050a86f91af02b54589729
CFTuser <deveshmaheshwari100@gmail.com>
2018-07-11 18:31:58 +0530

    changed in project and opprtunity

--- apps/users/serializer.py
+++ apps/users/serializer.py
@@ -1,35 +1,34 @@
 from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
 import re
-
+from django.contrib.auth.hashers import make_password
 from apps.users.models import MyUser
 
 
 
-class MyUserSerializer(serializers.Serializer):
+class MyUserSerializer(serializers.ModelSerializer):
     email = serializers.EmailField(validators=[UniqueValidator(queryset=MyUser.objects.all())])
+    password=serializers.CharField()
+
     class Meta:
         model = MyUser
         fields = (
             'email',
+            'username',
             'first_name',
             'middle_name',
+            'password',
             'last_name',
-            'password1',
-            'password2',
             'contact',
             'department',
             'designation',
             'gender',
         )
 
-    def create(validated_data):
+    def create(self,validated_data):
         print('*******************')
         print(validated_data)
-        user=MyUser.objects.create(validated_data)
-        print(user)
-        print('###############')
-        user.save()
+        user=MyUser.objects.create(**validated_data)
         return user
 
     def validate_first_name(self,firstname):
@@ -71,15 +70,19 @@ class MyUserSerializer(serializers.Serializer):
             else:
                 return salary
 
-    def validate_designation(self, design, dept):
-        if design==dept:
-            raise serializers.ValidationError("designation and department both can not be NA")
-        elif design=='Client' and dept!='NA':
-            raise serializers.ValidationError('not correct match of designation and department')
-        else:
-            return design
+    def validate_designation(self, design):
+        return design
+        # if design==dept:
+        #     raise serializers.ValidationError("designation and department both can not be NA")
+        # elif design=='Client' and dept!='NA':
+        #     raise serializers.ValidationError('not correct match of designation and department')
+        # else:
+        #     return design
+    def validate_password(self,passw):
+        passw=make_password(passw)
+        return passw
 
-#
+
 # {
 #     "email": "gdshg@gmail.com",
 #     "first_name": "devesh",
